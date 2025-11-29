ee0c205330b26af474ebb60d574a2b9b0ace0b9b
Sebastián Cabezas Ríos <sebastian.cabezas.r@gmail.com>
2025-11-28 22:30:38 -0300

    conexion con firebase OK

--- app/src/main/java/com/example/appandroidconfirebase/HomeActivity.java
+++ app/src/main/java/com/example/appandroidconfirebase/HomeActivity.java
@@ -8,18 +8,26 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private Button btnObtner, btnCambiar, btnLogout;
+    private Button btnObtener, btnCambiar, btnLogout;
     private TextView tvEstadoLed;
     private FirebaseAuth mAuth;
+
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,16 +35,26 @@ public class HomeActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
 
-        btnObtner = findViewById(R.id.btnObtener);
+        btnObtener = findViewById(R.id.btnObtener);
         btnCambiar = findViewById(R.id.btnCambiar);
         btnLogout = findViewById(R.id.btnLogout);
 
+        tvEstadoLed = findViewById(R.id.tvEstadoLed);
+
         mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
         btnLogout.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 logoutUser();
+            }
+        });
+
+        btnObtener.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                obtenerDataRTDB("estado_led");
             }
         });
 
@@ -60,5 +78,38 @@ public class HomeActivity extends AppCompatActivity {
         startActivity(intent);
         // 3.3 Se cierra la actividad actual
         finish();
+    }
+
+    private void obtenerDataRTDB(String clave){
+        //mostrarToast("Obteniendo data de RTDB...");
+        // obtener data de firebase realtime database
+        mDatabase.child(clave).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String mensaje = "";
+                if (snapshot.exists()) {
+                    Object value = snapshot.getValue();
+                    String data = String.valueOf(value);
+                    if (Boolean.parseBoolean(data)){
+                        mensaje = "ON";
+                    }else{
+                        mensaje = "OFF";
+                    }
+                    tvEstadoLed.setText(mensaje);
+                } else {
+                    mostrarToast("No se encontró el dato para la clave: " + snapshot.getKey());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                mostrarToast("Error al obtener data de RTDB: " + error.getMessage());
+            }
+        });
+
+    }
+
+    private void mostrarToast(String mensaje){
+        Toast.makeText(this, mensaje, Toast.LENGTH_LONG).show();
     }
 }
